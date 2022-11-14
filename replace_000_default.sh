224d396b35df38e84b4c6245424b30f89801ea55
Jive Turkey <75489987+j1v37u2k3y@users.noreply.github.com>
2022-11-14 13:39:10 -0500

    Create replace_000_default.sh

---
+++ replace_000_default.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+private_ip=$1
+server_name=$2
+mimic_server=$3
+phishing_server=$4
+sudo curl -sSL https://raw.githubusercontent.com/welikechips/bot-tools/master/000-default.conf \
+| sed 's|<REPLACE_PRIVATE_IP_ADDRESS>|'"${private_ip}"'|g' \
+| sed 's|<REPLACE_SERVER_NAME>|'"${server_name}"'|g' \
+| sed 's|<REPLACE_MIMIC_SERVER_NAME>|'"${mimic_server}"'|g' \
+| sed 's|<REPLACE_PHISHING_SERVER_NAME>|'"${phishing_server}"'|g' > /etc/apache2/sites-enabled/000-default-le-ssl.conf
