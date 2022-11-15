25ea2a1fdc06e9fdf80ff46abf748710c735875a
Jive Turkey <75489987+j1v37u2k3y@users.noreply.github.com>
2022-11-15 16:15:23 -0500

    Update replace_default_le_ssl.sh

--- replace_default_le_ssl.sh
+++ replace_default_le_ssl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 phishing_server=$1
+server_name=$2
 sudo curl -sSL https://raw.githubusercontent.com/welikechips/bot-tools/master/000-default-le-ssl.conf \
+| sed 's|<REPLACE_SERVER_NAME>|'"${server_name}"'|g' \
 | sed 's|<REPLACE_PHISHING_SERVER_NAME>|'"${phishing_server}"'|g' > /etc/apache2/sites-enabled/000-default-le-ssl.conf
