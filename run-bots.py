e8d5b93d5e2027bafd0d3250c750fddd906ea5d6
Thomas Norberg <tnorberg@thomasnorberg.com>
2022-11-28 16:27:28 -0500

    revert those changes.

--- run-bots.py
+++ run-bots.py
@@ -15,31 +15,18 @@ parser.add_argument('--job_index', type=int, default=0, help='The job index to r
 args = parser.parse_args()
 
 
-def check_bot(kwargs):
-    url = "https://{}/api/bots_bot/".format(kwargs.domain)
-    result = kwargs.client.get(url, params=kwargs.params, headers=kwargs.headers)
-
-    return json.loads(result.content)
-
-
-def check_jobs(kwargs):
-    url = "https://{}/api/bots_job/".format(kwargs.domain)
-    result = kwargs.client.get(url, params=kwargs.params, headers=kwargs.headers)
-
-    return json.loads(result.content)
-
-
 class CommandRunBots:
 
     def __init__(self):
-        pass
+       pass
 
     def handle(self, kwargs):
         client = requests.Session()
-        kwargs["headers"] = {"Authorization": "Token {}".format(kwargs.token)}
-        kwargs["params"] = {"guid": kwargs.guid, "finished": False}
-        kwargs["client"] = client
-        results = check_bot(kwargs)
+        headers = {"Authorization": "Token {}".format(kwargs.token)}
+        params = {"guid": kwargs.guid, "finished": False}
+        result = client.get("https://{}/api/bots_bot/".format(kwargs.domain), params=params, headers=headers)
+
+        results = json.loads(result.content)
 
         print(results)
         if int(results["count"]) == 1:
@@ -49,13 +36,15 @@ class CommandRunBots:
             if the_bot["finished"] is False and the_bot["in_process"] is False:
                 url = the_bot_url
                 data = {"in_process": True}
-                bot_update = client.patch(url, data=data, headers=kwargs.headers)
+                bot_update = client.patch(url, data=data, headers=headers)
                 print("bot_updated:")
                 print(bot_update.content)
             path = urllib.parse.urlparse(the_bot_url).path
             bot_id = os.path.basename(os.path.dirname(path))
-            kwargs["params"] = {"bot": bot_id, "finished": False, "in_process": False}
-            the_jobs = check_jobs(kwargs)
+            params = {"bot": bot_id, "finished": False, "in_process": False}
+            the_jobs_results = client.get("https://{}/api/bots_job/".format(kwargs.domain), params=params,
+                                          headers=headers)
+            the_jobs = json.loads(the_jobs_results.content)
             if int(the_jobs["count"]) >= 1:
                 os.system(the_bot["bot_task"])
                 # run 1 job
@@ -64,7 +53,7 @@ class CommandRunBots:
                 print(job["job_task"])
                 data = {"in_process": True}
                 job_url = job["url"].replace(kwargs.replace_domain, kwargs.domain)
-                client.patch(job_url, data=data, headers=kwargs.headers)
+                client.patch(job_url, data=data, headers=headers)
 
                 try:
                     sp = subprocess.Popen(job["job_task"],
@@ -78,17 +67,19 @@ class CommandRunBots:
                     out, err = sp.communicate()
                     print(out)
                     data = {"in_process": False, "finished": True, "result": out}
-                    client.patch(job_url, data=data, headers=kwargs.headers)
+                    client.patch(job_url, data=data, headers=headers)
                     total = the_bot["total"] + 1
-                    kwargs["params"] = {"bot": bot_id, "finished": False, "in_process": False}
-                    the_jobs = check_jobs(kwargs)
+                    params = {"bot": bot_id, "finished": False, "in_process": False}
+                    the_jobs_results = client.get("https://{}/api/bots_job/".format(kwargs.domain), params=params,
+                                                  headers=headers)
+                    the_jobs = json.loads(the_jobs_results.content)
 
                     if total > int(the_jobs["count"]):
                         data = {"total": total, "finished": True, "in_process": False}
-                        client.patch(the_bot_url, data=data, headers=kwargs.headers)
+                        client.patch(the_bot_url, data=data, headers=headers)
                     else:
                         data = {"total": total}
-                        client.patch(the_bot_url, data=data, headers=kwargs.headers)
+                        client.patch(the_bot_url, data=data, headers=headers)
                 except Exception as e:
                     if hasattr(e, 'message'):
                         message = e.message
@@ -96,12 +87,12 @@ class CommandRunBots:
                         message = e
                     print('an exception occurred!')
                     data = {"in_process": False, "finished": True, "result": message}
-                    client.patch(job_url, data=data, headers=kwargs.headers)
+                    client.patch(job_url, data=data, headers=headers)
 
             else:
                 url = the_bot_url
                 data = {"in_process": False, "finished": True}
-                client.patch(url, data=data, headers=kwargs.headers)
+                client.patch(url, data=data, headers=headers)
 
 
 command = CommandRunBots()
