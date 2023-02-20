df3209deb942b568c7be8e98f452cd1fed2c6f70
Thomas Norberg <tnorberg@thomasnorberg.com>
2023-02-20 10:04:55 -0500

    adding semaphore.

--- run-bots.py
+++ run-bots.py
@@ -17,14 +17,15 @@ args = parser.parse_args()
 
 class CommandRunBots:
 
-    def __init__(self):
-        pass
+    def __init__(self, kwargs):
+        self.headers = {"Authorization": "Token {}".format(kwargs.token)}
+        self.client = requests.Session()
+        self.args = kwargs
 
-    def handle(self, kwargs):
-        client = requests.Session()
-        headers = {"Authorization": "Token {}".format(kwargs.token)}
-        params = {"guid": kwargs.guid, "finished": False}
-        result = client.get("https://{}/api/bots_bot/".format(kwargs.domain), params=params, headers=headers)
+    def get_bot(self):
+        params = {"guid": self.args.guid, "finished": False}
+        result = self.client.get("https://{}/api/bots_bot/".format(self.args.domain), params=params,
+                                 headers=self.headers)
 
         results = json.loads(result.content)
 
@@ -32,72 +33,81 @@ class CommandRunBots:
         if int(results["count"]) == 1:
             bot_results = results["results"]
             the_bot = bot_results[0]
-            the_bot_url = the_bot["url"].replace(kwargs.replace_domain, kwargs.domain)
+            the_bot_url = the_bot["url"].replace(self.args.replace_domain, self.args.domain)
             if the_bot["finished"] is False and the_bot["in_process"] is False:
                 url = the_bot_url
                 data = {"in_process": True}
-                bot_update = client.patch(url, data=data, headers=headers)
+                bot_update = self.client.patch(url, data=data, headers=self.headers)
                 print("bot_updated:")
                 print(bot_update.content)
-            path = urllib.parse.urlparse(the_bot_url).path
-            bot_id = os.path.basename(os.path.dirname(path))
-            params = {"bot": bot_id, "finished": False, "in_process": False}
-            the_jobs_results = client.get("https://{}/api/bots_job/".format(kwargs.domain), params=params,
-                                          headers=headers)
-            the_jobs = json.loads(the_jobs_results.content)
-            if int(the_jobs["count"]) >= 1:
-                os.system(the_bot["bot_task"])
-                # run 1 job
-                job_url = None
-                try:
-                    if kwargs.job_index > len(the_jobs["results"]):
-                        job = the_jobs["results"][len(the_jobs["results"]) - 1]
-                    else:
-                        job = the_jobs["results"][kwargs.job_index]
-                    print(job["job_name"])
-                    print(job["job_task"])
-                    data = {"in_process": True}
-                    job_url = job["url"].replace(kwargs.replace_domain, kwargs.domain)
-                    client.patch(job_url, data=data, headers=headers)
-                    sp = subprocess.Popen(job["job_task"],
-                                          shell=True,
-                                          stdout=subprocess.PIPE,
-                                          stderr=subprocess.PIPE)
-                    rc = sp.wait()
+            if the_bot["semaphore"] is True:
+                # do something
+                pass
+            return the_bot, the_bot_url
 
-                    # Separate the output and error by communicating with sp variable.
-                    # This is similar to Tuple where we store two values to two different variables
-                    out, err = sp.communicate()
-                    print(out)
-                    data = {"in_process": False, "finished": True, "result": out}
-                    client.patch(job_url, data=data, headers=headers)
-                    total = the_bot["total"] + 1
-                    params = {"bot": bot_id}
-                    the_jobs_results = client.get("https://{}/api/bots_job/".format(kwargs.domain), params=params,
-                                                  headers=headers)
-                    the_jobs = json.loads(the_jobs_results.content)
-                    print(the_jobs)
-                    if total >= int(the_jobs["count"]):
-                        data = {"total": total, "finished": True, "in_process": False}
-                        client.patch(the_bot_url, data=data, headers=headers)
-                    else:
-                        data = {"total": total}
-                        client.patch(the_bot_url, data=data, headers=headers)
-                except Exception as e:
-                    if hasattr(e, 'message'):
-                        message = e.message
-                    else:
-                        message = e
-                    print(f'an exception occurred! {message}')
-                    if job_url is not None:
-                        data = {"in_process": False, "finished": True, "result": message}
-                        client.patch(job_url, data=data, headers=headers)
+    def handle(self):
+        the_bot, the_bot_url = self.get_bot()
+        path = urllib.parse.urlparse(the_bot_url).path
+        bot_id = os.path.basename(os.path.dirname(path))
+        params = {"bot": bot_id, "finished": False, "in_process": False}
+        the_jobs_results = self.client.get("https://{}/api/bots_job/".format(self.args.domain), params=params,
+                                           headers=self.headers)
+        the_jobs = json.loads(the_jobs_results.content)
+        if int(the_jobs["count"]) >= 1:
+            # run the bot task
+            # if there are replacements create them here.
+            os.system(the_bot["bot_task"])
+            # run 1 job
+            job_url = None
+            try:
+                if self.args.job_index > len(the_jobs["results"]):
+                    job = the_jobs["results"][len(the_jobs["results"]) - 1]
+                else:
+                    job = the_jobs["results"][self.args.job_index]
+                print(job["job_name"])
+                print(job["job_task"])
+                data = {"in_process": True}
+                job_url = job["url"].replace(self.args.replace_domain, self.args.domain)
+                self.client.patch(job_url, data=data, headers=self.headers)
+                sp = subprocess.Popen(job["job_task"],
+                                      shell=True,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
+                rc = sp.wait()
 
-            else:
-                url = the_bot_url
-                data = {"in_process": False, "finished": True}
-                client.patch(url, data=data, headers=headers)
+                # Separate the output and error by communicating with sp variable.
+                # This is similar to Tuple where we store two values to two different variables
+                out, err = sp.communicate()
+                print(out)
+                data = {"in_process": False, "finished": True, "result": out}
+                self.client.patch(job_url, data=data, headers=self.headers)
+                total = the_bot["total"] + 1
+                params = {"bot": bot_id}
+                the_jobs_results = self.client.get("https://{}/api/bots_job/".format(self.args.domain),
+                                                   params=params, headers=self.headers)
+                the_jobs = json.loads(the_jobs_results.content)
+                print(the_jobs)
+                if total >= int(the_jobs["count"]):
+                    data = {"total": total, "finished": True, "in_process": False}
+                    self.client.patch(the_bot_url, data=data, headers=self.headers)
+                else:
+                    data = {"total": total}
+                    self.client.patch(the_bot_url, data=data, headers=self.headers)
+            except Exception as e:
+                if hasattr(e, 'message'):
+                    message = e.message
+                else:
+                    message = e
+                print(f'an exception occurred! {message}')
+                if job_url is not None:
+                    data = {"in_process": False, "finished": True, "result": message}
+                    self.client.patch(job_url, data=data, headers=self.headers)
+
+        else:
+            url = the_bot_url
+            data = {"in_process": False, "finished": True}
+            self.client.patch(url, data=data, headers=self.headers)
 
 
-command = CommandRunBots()
-command.handle(args)
+command = CommandRunBots(args)
+command.handle()
